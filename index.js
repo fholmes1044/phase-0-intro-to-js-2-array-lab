d40982074ce08a13ba4d1d2649ab7b143da6ef1a
+ <+>
2023-08-21 17:05:49 -0400

    commit tree 962ac2661d4c907a631eac76276f4ad589d8d189

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+cats.unshift
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+const removeFirstCat = () => {
+     
+    return [...cats].splice(0,1)
+
+    
+}
